aa6febefa6a0c3a4f33857b4a902f307ac30e344
TenshiF <miguelangel@usf.edu>
2023-09-04 16:58:45 -0400

    Fully functional game of rps. First to 5.

--- script.js
+++ script.js
@@ -1,107 +1,112 @@
 let userWins = 0;
 let compWins = 0;
 
+const scoreBoard = document.createElement('div');
+const scoreCards = document.createElement('div');
+scoreCards.textContent = `${userWins} - ${compWins}`;
+const names = document.createElement('div');
+names.textContent = 'You - Computer';
+scoreBoard.appendChild(scoreCards);
+scoreBoard.appendChild(names);
+document.body.appendChild(scoreBoard);
+
+const rButton = document.createElement('button');
+rButton.textContent = 'Rock';
+document.body.appendChild(rButton);
+rButton.addEventListener('click', function () {
+    playRound(rButton.textContent, getComputerChoice());
+});
+
+const pButton = document.createElement('button');
+pButton.textContent = 'Paper';
+document.body.appendChild(pButton);
+pButton.addEventListener('click', function() {
+    playRound(pButton.textContent, getComputerChoice());
+});
+
+const sButton = document.createElement('button');
+sButton.textContent = 'Scissors';
+document.body.appendChild(sButton);
+sButton.addEventListener('click', function() {
+    playRound(sButton.textContent, getComputerChoice());
+});
+
 function getComputerChoice()
 {
     let choices = ["Rock", "Paper", "Scissors"]
     return choices[(Math.floor(Math.random() * choices.length))]
 }
 
-function playRound(playerSelection, computerSelection)
-{
-    console.log(playerSelection);
-    console.log(computerSelection);
+function playRound(playerSelection, computerSelection) {
     const round = document.createElement('div');
     document.body.appendChild(round);
-    if (playerSelection == "Rock")
-    {
-        if (computerSelection == "Rock")
-        {
-            round.textContent = ("It's a tie!")
-            return 0
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            round.textContent = ("It's a tie!");
         }
-        if (computerSelection == "Paper")
-        {
+        else if (computerSelection == "Paper") {
             compWins++;
-            round.textContent = ("Paper beats Rock! You lose!")
-            return -1
+            round.textContent = ("Paper beats Rock! You lose!");
         }
-        userWins++;
-        round.textContent = ("Rock beats Scissors! You win!")
-        return 1
-    }
-    if (playerSelection == "Paper")
-    {
-        if (computerSelection == "Paper")
-        {
-            round.textContent = ("It's a tie!")
-            return 0
-        }
-        if (computerSelection == "Rock")
-        {
+        else {
+            console.log('User Win');
             userWins++;
-            round.textContent = ("Paper beats Rock! You win!")
-            return 1
+            round.textContent = ("Rock beats Scissors! You win!");
         }
-        compWins++;
-        round.textContent = ("Scissors beats Paper! You lose!")
-        return -1
     }
-    if (playerSelection == "Scissors")
-    {
-        if (computerSelection == "Scissors")
-        {
-            round.textContent = ("It's a tie!")
-            return 0
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Paper") {
+            round.textContent = ("It's a tie!");
         }
-        if (computerSelection == "Paper")
-        {
+        else if (computerSelection == "Rock") {
             userWins++;
-            round.textContent = ("Scissors beats Paper! You win!")
-            return 1
+            console.log('User Win');
+            round.textContent = ("Paper beats Rock! You win!");
         }
-        compWins++;
-        round.textContent = ("Rock beats Scissors! You lose!")
-        return -1
+        else {
+            compWins++;
+            round.textContent = ("Scissors beats Paper! You lose!");
+        } 
+    }
+    else if (playerSelection == "Scissors") {
+        if (computerSelection == "Scissors") {
+            round.textContent = ("It's a tie!");
+        }
+        else if (computerSelection == "Paper") {
+            userWins++;
+            console.log('User Win');
+            round.textContent = ("Scissors beats Paper! You win!");
+        }
+        else {
+            compWins++;
+            round.textContent = ("Rock beats Scissors! You lose!");
+        }
+    }
+    updateScore();
+    if (userWins == 5) {
+        console.log('YOU WIN, THE BUTTONS SHOULD STOP WORKING NOW!')
+        const winner = document.createElement('div');
+        winner.textContent = "YOU WIN!";
+        document.body.appendChild(winner);
+        rButton.disabled = true;
+        pButton.disabled = true;
+        sButton.disabled = true;
+    }
+    else if (compWins == 5) {
+        console.log('THE COMPUTER WINS, THE BUTTONS SHOULD STOP WORKING NOW!');
+        const winner = document.createElement('div');
+        winner.textContent = "THE COMPUTER WINS!"
+        document.body.appendChild(winner);
+        rButton.disabled = true;
+        pButton.disabled = true;
+        sButton.disabled = true;
     }
 }
-
-const rButton = document.createElement('button');
-rButton.textContent = 'Rock';
-document.body.appendChild(rButton);
-
-const pButton = document.createElement('button');
-pButton.textContent = 'Paper';
-document.body.appendChild(pButton);
-
-const sButton = document.createElement('button');
-sButton.textContent = 'Scissors';
-document.body.appendChild(sButton);
 
 const div = document.createElement('div');
 
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        x = playRound(button.textContent, getComputerChoice());
-        const scoreUpdate = document.createElement('div');
-        scoreUpdate.textContent = `The score is now ${userWins} to ${compWins}`;
-        document.body.appendChild(scoreUpdate);
-        if (userWins == 5) {
-            const winner = document.createElement('div');
-            winner.textContent = "YOU WIN!";
-            document.body.appendChild(winner);
-            return;
-        }
-        else if (compWins == 5) {
-            const winner = document.createElement('div');
-            winner.textContent = "THE COMPUTER WINS!"
-            document.body.appendChild(winner);
-            return;
-        }
-
-    })
-    
-})
-
+function updateScore () {
+    scoreCards.textContent = `${userWins} - ${compWins}`;
+}
